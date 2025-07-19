ee66d9a44679b0addd5fdd2dbe8553312841b511
Narala-Girish-Reddy <naralagirishreddy@gmail.com>
2025-07-19 11:37:20 +0530

    BASIC PROGRAMS

---
+++ BASIC PROGRAMS/src/MyPackage/program2.java
@@ -0,0 +1,12 @@
+package MyPackage;
+
+public class program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String greeting = "Hello, World!";
+        System.out.println(greeting);
+
+	}
+
+}
